7224cb33fbc5793038f41e11168532a400a7c0d7
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-02-23 19:47:38 +0530

    Node Routing!

--- server.js
+++ server.js
@@ -5,16 +5,27 @@ const hostname = '127.0.0.1';
 const port = 8081;
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'application/json');
-  const names = ['francis', 'james', 'rob'];
-  const cars = {
-    name: 'Ford',
-    model: 'Fiesta',
-  };
-
-  const jsonData = JSON.stringify({ names, cars });
-  response.end(jsonData);
+  if (request.url === '/') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    const HTML = fs.readFileSync(`${__dirname}/index.html`);
+    response.end(HTML);
+  } else if (request.url === '/api/user') {
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    const user = JSON.stringify({
+      name: {
+        firstname: 'Harry',
+        lastname: 'Manchanda',
+      },
+    });
+    response.end(user);
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
 });
 
 server.listen(port, hostname, () => {
